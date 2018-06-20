88f11cd479ef9aa80eefaacc02ff7d9a7ce6be26
Aniruddh Gopale <anigopale@gmail.com>
2018-06-20 21:45:02 +0530

    render/append input, store input and calculated output

--- app.js
+++ app.js
@@ -3,26 +3,51 @@ var calcController = (function() {
     input: "",
     output: ""
   }
+
+  var calcOutput = function(input) {
+    var output = eval(input);
+
+    if(output) {
+      // update output only if input is valid
+      appData.output = output;
+    }
+  }
+
+  // public methods
+  return {
+    updateInput: function(input) {
+      // updates input and output
+      appData.input = appData.input + input;
+      calcOutput(appData.input);
+    }
+  }
+
 })();
 
 var UIController = (function() {
 
-  var renderInput = function(input) {
-    document.getElementById("input").textContent = input;
+  var renderInput = function(input, reset) {
+    if(reset) {
+      document.getElementById("input").textContent = input;
+    } else {
+      // if reset is false, append input
+      var currInput = document.getElementById("input").textContent;
+      document.getElementById("input").textContent = currInput + input;
+    }
   }
 
-  var renderOutput = function(output) {
+  var renderOutput = function(output, reset) {
     document.getElementById("output").textContent = output;
   }
 
   // public methods
   return {
-    displayInput: function(input) {
-      renderInput(input);
+    displayInput: function(input, reset) {
+      renderInput(input, reset);
     },
 
-    displayOutput: function(output) {
-      renderOutput(output);
+    displayOutput: function(output, reset) {
+      renderOutput(output, reset);
     }
   }
 })();
@@ -30,7 +55,7 @@ var UIController = (function() {
 
 var appController = (function(calcCtrl, UICtrl) {
 
-  // custom for each function
+  // custom forEach function
   var nodeListForEach = function(list, callback) {
     for (var i = 0; i < list.length; i++) {
       callback(list[i], i);
@@ -46,12 +71,14 @@ var appController = (function(calcCtrl, UICtrl) {
         if (buttonContent === "DEL") {
           // delete number
         } else if (buttonContent === "=") {
-          // show output
+          // show output (set output as the new input)
         } else {
           // update input
           UICtrl.displayInput(buttonContent);
+          calcCtrl.updateInput(buttonContent);
 
           // calculate output
+
 
           // update output
         }
@@ -63,8 +90,8 @@ var appController = (function(calcCtrl, UICtrl) {
   // public methods
   return {
     init: function() {
-      UICtrl.displayInput("");
-      UICtrl.displayOutput("");
+      UICtrl.displayInput("", true);
+      UICtrl.displayOutput("", true);
 
       setupEventListeners();
     }
